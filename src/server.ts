06b1b7e27e6e2083283e59d0ffba4c6f0c1e5a74
Jose David <lujan9495@gmail.com>
2025-04-24 16:32:58 -0500

    Conectando la aplicación de Express a una BD y colocando la conexión en variables de entorno

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,20 @@
 import express from "express";
 import router from "./routes"
+import db from "./config/db";
 
+// Conectar DB
+async function connectDB() {
+  try {
+    await db.authenticate()
+    db.sync()
+    console.log("Conexión exitosa")
+  } catch (error) {
+    console.log(error)
+    console.log("Error al conectar a la BD")
+  }
+}
+
+connectDB()
 const server = express()
 
 server.use('/api/products', router)
