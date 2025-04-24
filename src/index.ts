f7cda080aa57c81c745f98cd103969253f34a919
Jose David <lujan9495@gmail.com>
2025-04-23 20:41:07 -0500

    Probando las rutas en ThunderClient

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-import { sumar } from "./server";
-sumar()
+import server from "./server";
+
+server.listen(8080, () => {
+  console.log("Listen on port 8080")
+})
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,26 @@
-export function sumar () {
-  console.log(2 + 2) 
-}
+import express from "express";
+
+const server = express()
+
+// Routing 
+server.get('/', (req, res) => {
+  res.json("Desde get")
+})
+
+server.post('/', (req, res) => {
+  res.json("Desde post")
+})
+
+server.put('/', (req, res) => {
+  res.json("Desde put")
+})
+
+server.patch('/', (req, res) => {
+  res.json("Desde patch")
+})
+
+server.delete('/', (req, res) => {
+  res.json("Desde DELETE")
+})
+
+export default server
