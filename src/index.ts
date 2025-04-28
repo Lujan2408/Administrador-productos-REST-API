551e4d2f2263b7b3e60d584674abe07f7b93ad39
Jose David <lujan9495@gmail.com>
2025-04-28 14:38:13 -0500

    Creando una funcion para crear productos y utilizando handlers

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import colors from "colors"
 import server from "./server";
 
 const PORT = process.env.PORT || 4000
-server.listen(8080, () => {
+server.listen(4000, () => {
   console.log(colors.cyan(`Listen on port ${PORT}`))
 })
