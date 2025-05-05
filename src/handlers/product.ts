395cf2708a35a6217dbe173db076e8c92b55a542
Jose David <lujan9495@gmail.com>
2025-05-05 14:35:52 -0500

    Validando Crear Producto en el servidor con express validator

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,10 +1,31 @@
 import { Request, Response } from "express"
+import { check, validationResult } from "express-validator"
 import Product from "../models/Product.model"
 
 // Siempre que trabajemos con los modelos las funciones deben ser asincronas 
 
-export const createProduct = async (req, res) => {
+export const createProduct = async (req : Request, res : Response) => {
   
+  // Validación
+  await check('name')
+          .notEmpty().withMessage('El nombre del producto no puede estar vacío')
+          .run(req)
+  await check('price')
+          .isNumeric().withMessage('Valor no válido')
+          .notEmpty().withMessage('El precio del producto no puede estar vacío')
+          .custom( value => value > 0).withMessage('Precio no válido')
+          .run(req)
+
+    let errors = validationResult(req)
+    if(!errors.isEmpty()){
+      res.json({
+          "message": errors.array(),
+          "status": 400
+      })
+
+      return
+  }
+
   const product = await Product.create(req.body)
   res.json({data: product })
 }
