6eed9fbc2adc9e49176f27e1c5ea10e6e3e80866
Jose David <lujan9495@gmail.com>
2025-05-08 14:32:01 -0500

    Creando el Endpoint para obtener todos los productos

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -3,6 +3,22 @@ import Product from "../models/Product.model"
 
 // Siempre que trabajemos con los modelos las funciones deben ser asincronas 
 
+//Obtener productos
+export const getProducts = async (req : Request, res : Response) => {
+  try {
+    const products = await Product.findAll({
+        order: [
+          ['price', 'DESC']
+        ],
+        attributes: { exclude: ['createdAt', 'updatedAt', 'availability']}
+    })
+    res.json({data: products})
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// Crear productos
 export const createProduct = async (req : Request, res : Response) => {
   try {
     const product = await Product.create(req.body)
